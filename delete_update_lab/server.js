d697ae692e4d3bcdadc8d1724bc2dbb6eb7f295a
Mariana <marianac94@hotmail.com>
2018-10-10 12:36:04 -0500

    delete button working

--- delete_update_lab/server.js
+++ delete_update_lab/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const People = require('./models/people');
 
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 
 app.get('/', (req, res) => {
   res.send('yepppp, workin just fine!')
@@ -10,8 +13,17 @@ app.get('/', (req, res) => {
 app.get('/people', (req, res) => {
   res.render('index.ejs', {
     classroom: People
-  })
-})
+  });
+});
+
+app.delete('/people/:index', (req, res) => {
+  People.splice(req.params.index, 1);
+  res.redirect('/people')
+});
+
+
+
+
 
 
 app.listen(3000, (req, res) => {
