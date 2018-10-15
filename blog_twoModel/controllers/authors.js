d976d5f125050d17636d19f3c6b1c094c045b73c
Mariana <marianac94@hotmail.com>
2018-10-15 12:05:20 -0500

    added delete button

--- blog_twoModel/controllers/authors.js
+++ blog_twoModel/controllers/authors.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-const Authors = require('../models/authors');
+const Author = require('../models/authors');
 
 router.get('/', (req, res) => {
   Author.find({}, (err, foundAuthors) => {
     res.render('authors/index.ejs', {
       authors: foundAuthors
     });
-  });
-});
+  })
+ });
 
 router.get('/new', (req, res) => {
   res.render('authors/new.ejs')
 });
 
 router.get('/:id', (req, res) => {
-  Author findById(req.params.id, (err, authorFound) => {
+  Author.findById(req.params.id, (err, authorFound) => {
     res.render('authors/show.ejs', {
-      authors: authorFound
+      author: authorFound
     })
   })
 })
@@ -32,6 +32,11 @@ router.post('/', (req, res) => {
     }
   });
 });
+
+router.delete('/:id', (req, res) => {
+  Author.findByIdAndRemove(req.params.id, (err, deleted))
+    res.redirect('/authors')
+})
 
 
 
--- blog_twoModel/server.js
+++ blog_twoModel/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 require('./db/db');
+
 const authorsController = require('./controllers/authors');
+
 
 // where every route will start with /authors
 app.use('/authors', authorsController);
-
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
 
 
 app.get('/', (req, res) => {
